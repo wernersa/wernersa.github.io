122dd6115f29a94d363173b38ffdacdad8fa57d3
Werner Sævland <werner.saevland@gmail.com>
2022-07-30 11:08:55 +0800

    Fix GitHub pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  baseURL: '/werner.best/',
+  ssr: 'false',
   target: 'static',
   modules: [
     '@nuxt/content',
