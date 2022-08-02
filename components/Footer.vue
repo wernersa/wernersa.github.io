3294d83aac8ac56029d168962da31216db387da4
Werner Sævland <werner.saevland@gmail.com>
2022-08-02 14:36:20 +0800

    Increase contrast for accessibility

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-6">
-    <span class="block text-sm text-center text-slate-400 dark:text-slate-500"><Icon name="ph:copyleft" style="width: 0.875rem; height: 0.875rem;" :inline="true" /> 2022 <a href="https://github.com/wernersa/wernersa.github.io" class="hover:underline">Public Domain</a>. All Rights Reversed.
+    <span class="block text-sm text-center text-slate-600 dark:text-slate-400"><Icon name="ph:copyleft" style="width: 0.875rem; height: 0.875rem;" :inline="true" /> 2022 <a href="https://github.com/wernersa/wernersa.github.io" class="hover:underline">Public Domain</a>. All Rights Reversed.
     </span>
   </div>
 </template>
