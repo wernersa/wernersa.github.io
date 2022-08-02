247aa03f2617e651ae145a7d2d1c71be54f810d8
Werner Sævland <werner.saevland@gmail.com>
2022-08-02 10:43:38 +0800

    Fix github repository link

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="py-6">
-    <!-- TODO: Check that link in footer is working -->
-    <span class="block text-sm text-center text-slate-400 dark:text-slate-500"><Icon name="ph:copyleft" style="width: 0.875rem; height: 0.875rem;" :inline="true" /> 2022 <a href="https://github.com/wernersa/werner.best" class="hover:underline">Public Domain</a>. All Rights Reversed.
+    <span class="block text-sm text-center text-slate-400 dark:text-slate-500"><Icon name="ph:copyleft" style="width: 0.875rem; height: 0.875rem;" :inline="true" /> 2022 <a href="https://github.com/wernersa/wernersa.github.io" class="hover:underline">Public Domain</a>. All Rights Reversed.
     </span>
   </div>
 </template>
